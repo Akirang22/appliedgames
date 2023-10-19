9b0243ba2545aee4369d34ac8cf7e4a10698d9f5
Mastermoni22 <tim02.dijkstra@outlook.com>
2023-10-19 11:24:53 +0200

    mouse look
    Added:
    - look around by using mouse

---
+++ Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField]
+    private float mouseSensitivity = 100f;
+    [SerializeField]
+    private Transform playerBody;
+
+    private float rotationX = 0f;
+    [SerializeField]
+    private float rotationXLimit = 90f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -rotationXLimit, rotationXLimit);
+
+        transform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
